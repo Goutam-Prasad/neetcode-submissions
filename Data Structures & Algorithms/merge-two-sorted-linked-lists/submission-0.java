5a39702bee085777c078ee4c080e4e7e2ccced8d
Goutam Prasad <81437356+Goutam-Prasad@users.noreply.github.com>
2026-05-10 07:26:25 +0530

    Add: merge-two-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummy = new ListNode(-1);
+        ListNode result = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                result.next = list1;
+                list1 = list1.next;
+            } else {
+                result.next = list2;
+                list2 = list2.next;
+            }
+            result = result.next;
+        }
+
+        if (list1 != null) result.next = list1;
+        if (list2 != null) result.next = list2;
+
+        return dummy.next;  // first real node
+    }
+}
